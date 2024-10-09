0158d809e69d78041fbd76441c13ebb10c79367f
Rey Garcia <jreygarcia24@gmail.com>
2024-10-09 22:54:41 +0800

    adjust padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row h-full max-w-[90rem] justify-center w-full">
+    <main className="flex flex-col md:flex-row h-full max-w-[90rem] justify-center w-full lg:py-3 lg:px-20 xl:px-0">
       <div className="min-h-80 md:min-h-full md:min-w-[8.5rem] xl:min-w-[14rem] relative ">
         <div className="p-5 relative md:mt-0 md:flex items-center w-full h-full top-16 md:top-0">
           <Form />
@@ -14,7 +14,7 @@ export default function Home() {
           src="/banner.png"
           width={1000}
           height={760}
-          className="h-full md:w-full md:h-[90%] object-cover md:rounded-xl"
+          className="h-full md:w-full object-cover md:rounded-xl"
           alt="Living Room"
         />
       </div>
