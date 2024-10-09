2c36737ac0d93f5fa19777e051fb3a547f6dcb3c
Rey Garcia <jreygarcia24@gmail.com>
2024-10-09 17:31:18 +0800

    adjust padding.

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="flex flex-col md:flex-row h-full max-w-7xl justify-center w-full">
       <div className="min-h-80 md:min-h-full md:min-w-[8.5rem] xl:min-w-[14rem] relative ">
-        <div className="p-5 absolute md:relative mt-8 md:mt-0 md:flex items-center w-full h-full">
+        <div className="p-5 relative md:mt-0 md:flex items-center w-full h-full top-16 md:top-0">
           <Form />
         </div>
       </div>
-      <div className="h-full flex w-full items-center md:p-6">
+      <div className="h-full flex w-full items-center md:p-6 min-h-[30rem]">
         <Image
           src="/banner.png"
           width={1000}
